5cc4526be7bd78bb69b3a3b3c4691ce304c67840
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-15 01:26:31 -0300

    middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+// modules
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
+// express app
 const app = express()
+// middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+// routes
 app.listen(process.env.PORT,()=>{console.log(`listening at port ${process.env.PORT}`)})
 app.get('/',(req,res)=>{
     res.json({'msg':'hello!'})
