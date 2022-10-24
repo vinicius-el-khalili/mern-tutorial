ac53c8cbad5690e8e9c53c7125d933f54e3f6403
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-24 17:44:35 -0300

    react-routed Link anchor tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 // Pages and Components
 import Home from './pages/Home'
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="pages">
           <Routes>
             <Route
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+const Navbar = ()=>{
+    return(
+            <div className="navbar">
+                <Link to="/">
+                    <h1>Home</h1>
+                </Link>   
+            </div>
+    )
+}
+export default Navbar
