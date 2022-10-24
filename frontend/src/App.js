e6dcfa949fc0ea12d7243719284a2dba08c0b0db
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-24 15:59:04 -0300

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// Pages and Components
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}>
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
