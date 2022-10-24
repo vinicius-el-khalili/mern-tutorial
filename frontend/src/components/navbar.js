4c2f6966e4dfcd3e131acead1faec1562b2583cb
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-24 18:01:11 -0300

    try to fetch API but fail but fail because of the CORS policy

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 const Navbar = ()=>{
     return(
-            <div className="navbar">
-                <Link to="/">
-                    <h1>Home</h1>
-                </Link>   
-            </div>
+        <div className="navbar">
+            <Link to="/">
+                <h1>Home</h1>
+            </Link>
+        </div>
     )
 }
 export default Navbar
